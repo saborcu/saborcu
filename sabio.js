b7255098fd228e69bfaffb7177b1428bad5dadcf
saborcu <rlpeze123@gmail.com>
2017-06-05 15:32:21 -0700

    Create sabio.js

---
+++ sabio.js
@@ -0,0 +1,36 @@
+// JavaScript source code
+var sabio = {
+    utilities: {}
+    , layout: {}
+    , page: {
+        handlers: {
+        }
+        , startUp: null
+    }
+    , services: {}
+    , ui: {}
+
+};
+sabio.moduleOptions = {
+    APPNAME: "SabioApp"
+    , extraModuleDependencies: []
+    , runners: []
+    , page: sabio.page//we need this object here for later use
+}
+
+
+sabio.layout.startUp = function () {
+
+    console.debug("sabio.layout.startUp");
+
+    //this does a null check on sabio.page.startUp
+    if (sabio.page.startUp) {
+        console.debug("sabio.page.startUp");
+        sabio.page.startUp();
+    }
+};
+sabio.APPNAME = "SabioApp";//legacy
+
+
+
+$(document).ready(sabio.layout.startUp);
